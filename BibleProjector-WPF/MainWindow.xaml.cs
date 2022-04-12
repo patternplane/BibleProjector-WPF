92e04b9bdd6fc34967b0f08925db53978d3dbca2
patternplane <bss011202@naver.com>
2022-04-12 18:56:58 +0900

    [ADD] 성경 선택버튼 제작 / 장 절 선택기능 생성

--- BibleProjector-WPF/MainWindow.xaml.cs
+++ BibleProjector-WPF/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.ComponentModel;
+
 namespace BibleProjector_WPF
 {
     /// <summary>
@@ -20,8 +22,18 @@ namespace BibleProjector_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        // 프로그램 모듈
         Database db;
         Powerpoint ppt;
+
+        // 성경
+        // 신구약 버튼들
+        BindingList<BibleTitleData> OldTestButtons;
+        BindingList<BibleTitleData> NewTestButtons;
+        // 장 번호 리스트
+        BindingList<int> ChapterNumberList;
+        // 절 번호 리스트
+        BindingList<int> VerseNumberList;
 
         public MainWindow()
         {
@@ -29,11 +41,58 @@ namespace BibleProjector_WPF
             ppt = new Powerpoint();
 
             InitializeComponent();
+
+            BibleInitialize();
         }
 
         public void programOut()
         {
             db.Disconnect();
         }
+
+        void BibleInitialize()
+        {
+            SetBibleButtons();
+            SetBibleChapterVerse();
+        }
+
+        //========================================= 성경 ============================================
+
+        // 세팅
+
+        void SetBibleButtons()
+        {
+            OldTestButtons = new BindingList<BibleTitleData>();
+            Bible_OldTestButtons_ItemsControl.ItemsSource = OldTestButtons;
+
+            NewTestButtons = new BindingList<BibleTitleData>();
+            Bible_NewTestButtons_ItemsControl.ItemsSource = NewTestButtons;
+
+            BibleTitleData[] titleData = db.getBibleTitleData();
+
+            for (int i = 0; i < 39; i++)
+                OldTestButtons.Add(titleData[i]);
+            for (int i = 39; i < 66; i++)
+                NewTestButtons.Add(titleData[i]);
+        }
+
+        void SetBibleChapterVerse()
+        {
+            ChapterNumberList = new BindingList<int>();
+            VerseNumberList = new BindingList<int>();
+
+            Bible_Chapter_ListBox.ItemsSource = ChapterNumberList;
+            Bible_Verse_ListBox.ItemsSource = VerseNumberList;
+        }
+
+        // 성경선택버튼 조작
+
+        void BibleButton_Click(object sender, RoutedEventArgs e)
+        {
+            string BibleNumber = ((Button)sender).Tag.ToString().PadLeft(2, '0');
+        }
+
+        //
     }
+
 }
---
+++ BibleProjector-WPF/module/BibleButtonData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibleProjector_WPF
+{
+    class BibleTitleData
+    {
+        public string BookTitle { get; set; }
+        public int BookNumber { get; set; }
+
+        public BibleTitleData(String ButtonName, int BookNumber)
+        {
+            this.BookTitle = ButtonName;
+            this.BookNumber = BookNumber;
+        }
+    }
+}
