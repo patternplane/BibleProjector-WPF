6088408a26fcad1f7ff3580eeea99cc0c9acc0f0
patternplane <bss011202@naver.com>
2022-05-06 15:22:19 +0900

    [FEAT] 프로그램은 단 한번만 실행될 수 있습니다

--- BibleProjector-WPF/App.xaml.cs
+++ BibleProjector-WPF/App.xaml.cs
@@ -13,6 +13,26 @@ namespace BibleProjector_WPF
     /// </summary>
     public partial class App : Application
     {
+
+        System.Threading.Mutex mutex;
+        public const string PROGRAM_FULLNAME = "WorshipProjector-WPF-BSS";
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            bool createdNew = false;
+            mutex = new System.Threading.Mutex(true, PROGRAM_FULLNAME, out createdNew);
+
+            if (createdNew)
+            {
+                base.OnStartup(e);
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("프로그램은 한번만 실행하세요!", "프로그램이 이미 작동중", MessageBoxButton.OK, MessageBoxImage.Error);
+                Environment.Exit(0);
+            }
+        }
+
         void ProgramExit(object sender, ExitEventArgs e)
         {
             Console.WriteLine("프로그램 정상 종료");
