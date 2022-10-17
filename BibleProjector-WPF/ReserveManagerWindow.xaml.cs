e3e005e839aee335eedefcd88db5d17f72deb20f
patternplane <bss011202@naver.com>
2022-10-17 14:32:18 +0900

    [XAML] 예약창 생성

---
+++ BibleProjector-WPF/ReserveManagerWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using BibleProjector_WPF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System.ComponentModel;
+
+namespace BibleProjector_WPF
+{
+    /// <summary>
+    /// ReserveManagerWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ReserveManagerWindow : Window
+    {
+        public ReserveManagerWindow()
+        {
+            InitializeComponent();
+        }
+        public ReserveManagerWindow(System.Collections.IEnumerable reservdata)
+        {
+            InitializeComponent();
+
+            BibleReserveListBox2.ItemsSource = reservdata;
+            BibleReserveListBox2.DisplayMemberPath = "DisplayData";
+        }
+    }
+}
