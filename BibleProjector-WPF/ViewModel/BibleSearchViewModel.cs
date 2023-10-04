f0e8f882dc268d494a4f3f9df43bfbed3b26e8bc
patternplane <bss011202@naver.com>
2023-10-04 16:35:07 +0900

    [TEMP] 성경 검색기능 추가 중 임시저장 1. 자동완성 탭 추가 2. 검색기능 추가중

---
+++ BibleProjector-WPF/ViewModel/BibleSearchViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace BibleProjector_WPF.ViewModel
+{
+    class BibleSearchViewModel : INotifyPropertyChanged
+    {
+
+        string searchText_in = "";
+        public string searchText { get { return searchText_in; } set { searchText_in = value; } }
+
+        public ObservableCollection<string> searchList { get; set; } = new ObservableCollection<string>(new string[] { "hello"});
+
+        public bool popupOpen { get; set; } = false;
+
+        // ====================================== 메소드
+
+        public void searchTextChanged()
+        {
+            
+
+            popupOpen = true;
+            NotifyPropertyChanged("popupOpen");
+        }
+
+        // ====================================== INotifyPropertyChanged 인터페이스 구현
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
