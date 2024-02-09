688bd1b575167fcc6e339bf014b217bd9c4c90f5
patternplane <bss011202@naver.com>
2024-02-09 09:12:56 +0900

    [FEAT] 예약목록의 드래그 앤 드롭 조작 도입 - 기반 설계중

--- BibleProjector-WPF/View/MainPage/ControlView.xaml.cs
+++ BibleProjector-WPF/View/MainPage/ControlView.xaml.cs
@@ -24,5 +24,29 @@ namespace BibleProjector_WPF.View.MainPage
         {
             InitializeComponent();
         }
+
+        private void testMouseMove(object sender, MouseEventArgs e)
+        {
+            ListBoxItem obj = sender as ListBoxItem;
+
+            if (obj != null && e.LeftButton == MouseButtonState.Pressed)
+            {
+                DataObject data = new DataObject();
+                data.SetData(typeof(ListBoxItem), obj);
+                DragDrop.DoDragDrop(obj,
+                                     data,
+                                     DragDropEffects.Move);
+                Console.WriteLine("드래그의 시작");
+            }
+        }
+        
+        private void testDragOver(object sender, DragEventArgs e)
+        {
+            
+            Point pos = e.GetPosition((IInputElement)sender);
+            Console.WriteLine(e.Data.GetData(typeof(ListBoxItem)).ToString());
+            ((ListBoxItem)e.Data.GetData(typeof(ListBoxItem))).RenderTransform = new TranslateTransform(pos.X, pos.Y);
+            //Console.WriteLine("x : {0}, y : {1}",pos.X,pos.Y);
+        }
     }
 }
