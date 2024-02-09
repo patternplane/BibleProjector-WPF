a7b507ff7a5d4120958d27104f37dc5bbc10c917
patternplane <bss011202@naver.com>
2024-02-09 22:56:52 +0900

    [FEAT] 옵션 등장 애니메이션 개선 - 이벤트에만 반응하여 닫기과정에 버벅임이 발생하는 등의 문제가 있었음 - 특정 속성에 반응하도록 바꾸어 애니메이션 과정에 끊김/반복이 없도록 개선 - EasingFunction을 적용하여 매끄러운 애니메이션 동작 추가 - 그러나 추후 옵션 View를 연결하는 부분은 개선 필요(MainOption과의 강한 관계를 끊을 것)

--- BibleProjector-WPF/View/MainPage/MainView.xaml.cs
+++ BibleProjector-WPF/View/MainPage/MainView.xaml.cs
@@ -24,5 +24,23 @@ namespace BibleProjector_WPF.View.MainPage
         {
             InitializeComponent();
         }
+
+        System.Reflection.PropertyInfo CPullOptionBarProperty = null;
+        void EH_OptionButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (CPullOptionBarProperty == null)
+                CPullOptionBarProperty = (this.DataContext).GetType().GetProperty("CPullOptionBar");
+
+            ((ICommand)(CPullOptionBarProperty.GetValue(this.DataContext))).Execute(null);
+        }
+
+        System.Reflection.PropertyInfo CPushOptionBarProperty = null;
+        void EH_OptionBarMouseLeave(object sender, RoutedEventArgs e)
+        {
+            if (CPushOptionBarProperty == null)
+                CPushOptionBarProperty = (this.DataContext).GetType().GetProperty("CPushOptionBar");
+
+            ((ICommand)(CPushOptionBarProperty.GetValue(this.DataContext))).Execute(null);
+        }
     }
 }
--- BibleProjector-WPF/ViewModel/MainPage/VMMain.cs
+++ BibleProjector-WPF/ViewModel/MainPage/VMMain.cs
@@ -7,7 +7,39 @@ using System.Windows.Input;
 
 namespace BibleProjector_WPF.ViewModel.MainPage
 {
-    public class VMMain
+    public class VMMain : NotifyPropertyChanged
     {
+        public ICommand CPullOptionBar { get; set; }
+        public ICommand CPushOptionBar { get; set; }
+
+        bool _isOptionBarOut;
+        public bool isOptionBarOut
+        {
+            get
+            {
+                return _isOptionBarOut;
+            }
+            set
+            {
+                _isOptionBarOut = value;
+                OnPropertyChanged("isOptionBarOut");
+            }
+        }
+
+        public VMMain()
+        {
+            CPullOptionBar = new RelayCommand(param => pullOptionBar());
+            CPushOptionBar = new RelayCommand(param => pushOptionBar());
+        }
+
+        void pullOptionBar()
+        {
+            isOptionBarOut = true;
+        }
+
+        void pushOptionBar()
+        {
+            isOptionBarOut = false;
+        }
     }
 }
