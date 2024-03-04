2454fc76f178d0e1313f61d755851656d5d27c90
patternplane <bss011202@naver.com>
2024-03-04 21:43:38 +0900

    [XAML] 성경 컨트롤러의 ViewModel과 View 연결 작업중

--- BibleProjector-WPF/View/MainPage/ShowControler.xaml.cs
+++ BibleProjector-WPF/View/MainPage/ShowControler.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,6 +24,129 @@ namespace BibleProjector_WPF.View.MainPage
         public ShowControler()
         {
             InitializeComponent();
+        }
+
+        // 언제 숫자입력값을 다시 초기화해야 할지 곰곰히 생각해봐야 할 듯 합니다.
+        /*void Window_Activated(object sender, EventArgs e)
+        {
+            VM_BibleControl.NumInput_Remove();
+        }*//*
+
+        void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.D0 && e.Key <= Key.D9
+                || e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+            {
+                VM_BibleControl.NumInput(KeyToNum(e.Key));
+                return;
+            }
+
+            switch (e.Key)
+            {
+                case Key.Up:
+                case Key.Right:
+                    VM_BibleControl.RunNextPage();
+                    break;
+                case Key.Down:
+                case Key.Left:
+                    VM_BibleControl.RunPreviousPage();
+                    break;
+                case Key.Enter:
+                    VM_BibleControl.NumInput_Enter();
+                    break;
+            }
+
+            VM_BibleControl.NumInput_Remove();
+        }
+        int KeyToNum(Key key)
+        {
+            if (key >= Key.D0 && key <= Key.D9)
+                return (key - Key.D0);
+            else if (key >= Key.NumPad0 && key <= Key.NumPad9)
+                return (key - Key.NumPad0);
+            else
+                return -1;
+        }*/
+
+        // ========== BindingProperties ==========
+        
+        System.Reflection.PropertyInfo _CDisplayOnOffProperty = null;
+        ICommand getCDisplayOnOffProperty(object obj)
+        {
+            if (_CDisplayOnOffProperty == null)
+                _CDisplayOnOffProperty = obj.GetType().GetProperty("CDisplayOnOff")
+                    ?? throw new Exception("Binding Error");
+
+            return (ICommand)_CDisplayOnOffProperty.GetValue(obj);
+        }
+
+        System.Reflection.PropertyInfo _CTextShowHideProperty = null;
+        ICommand getCTextShowHideProperty(object obj)
+        {
+            if (_CTextShowHideProperty == null)
+                _CTextShowHideProperty = obj.GetType().GetProperty("CTextShowHide")
+                    ?? throw new Exception("Binding Error");
+
+            return (ICommand)_CTextShowHideProperty.GetValue(obj);
+        }
+
+        System.Reflection.PropertyInfo _CGoNextPageProperty = null;
+        ICommand getCGoNextPageProperty(object obj)
+        {
+            if (_CGoNextPageProperty == null)
+                _CGoNextPageProperty = obj.GetType().GetProperty("CGoNextPage")
+                    ?? throw new Exception("Binding Error");
+
+            return (ICommand)_CGoNextPageProperty.GetValue(obj);
+        }
+
+        System.Reflection.PropertyInfo _CGoPreviousPageProperty = null;
+        ICommand getCGoPreviousPageProperty(object obj)
+        {
+            if (_CGoPreviousPageProperty == null)
+                _CGoPreviousPageProperty = obj.GetType().GetProperty("CGoPreviousPage")
+                    ?? throw new Exception("Binding Error");
+
+            return (ICommand)_CGoPreviousPageProperty.GetValue(obj);
+        }
+
+        System.Reflection.PropertyInfo _CSetDisplayTopMostProperty = null;
+        ICommand getCSetDisplayTopMostProperty(object obj)
+        {
+            if (_CSetDisplayTopMostProperty == null)
+                _CSetDisplayTopMostProperty = obj.GetType().GetProperty("CSetDisplayTopMost")
+                    ?? throw new Exception("Binding Error");
+
+            return (ICommand)_CSetDisplayTopMostProperty.GetValue(obj);
+        }
+
+        // ========== EventHandler ==========
+
+        void EH_GoPrevious(object sender, RoutedEventArgs e)
+        {
+            getCGoPreviousPageProperty(this.DataContext).Execute(null);
+        }
+
+        void EH_GoNext(object sender, RoutedEventArgs e)
+        {
+            getCGoNextPageProperty(this.DataContext).Execute(null);
+        }
+
+        void PageListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox ControlListBox = (ListBox)sender;
+            ListBoxItem SelectedItem = (ListBoxItem)ControlListBox.ItemContainerGenerator.ContainerFromItem(ControlListBox.SelectedItem);
+            SelectedItem?.Focus();
+        }
+
+        private void EH_DisplayOffButtonClick(object sender, RoutedEventArgs e)
+        {
+            getCDisplayOnOffProperty(this.DataContext).Execute(!((ToggleButton)sender).IsChecked);
+        }
+
+        private void EH_TextShowButtonClick(object sender, RoutedEventArgs e)
+        {
+            getCTextShowHideProperty(this.DataContext).Execute(!((ToggleButton)sender).IsChecked);
         }
     }
 }
--- BibleProjector-WPF/ViewModel/MainPage/VMShowControler.cs
+++ BibleProjector-WPF/ViewModel/MainPage/VMShowControler.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace BibleProjector_WPF.ViewModel.MainPage
 {
@@ -11,5 +13,176 @@ namespace BibleProjector_WPF.ViewModel.MainPage
         public string Title1 { get; set; }
         public string Title2 { get; set; }
         public ShowContentType ContentType { get; set; }
+
+        // ================================================ 속성 ================================================
+
+        public ICommand CDisplayOnOff { get; set; }
+        public ICommand CTextShowHide { get; set; }
+        public ICommand CGoNextPage { get; set; }
+        public ICommand CGoPreviousPage { get; set; }
+        public ICommand CSetDisplayTopMost { get; set; }
+
+        // 성경 페이지 리스트박스
+        public ObservableCollection<string> BiblePages { get; set; }
+        private int CurrentPageIndex_in;
+        public int CurrentPageIndex
+        {
+            get { return CurrentPageIndex_in; }
+            set
+            {
+                if (CurrentPageIndex_in != value)
+                {
+                    CurrentPageIndex_in = value;
+                    if (CurrentPageIndex_in != -1)
+                        // 페이지 변경 처리
+                        Powerpoint.Bible.Change_VerseContent(verse.ToString(), BiblePages[CurrentPageIndex_in], CurrentPageIndex_in == 0);
+                }
+            }
+        }
+
+        // 이전 페이지 넘기는 동작 설정
+        public bool preview_GoLastPage { get; set; } = true;
+
+        string Kjjeul_in;
+        string Kjjeul
+        {
+            get { return Kjjeul_in; }
+            set
+            {
+                Kjjeul_in = value;
+                bible_display = Database.getTitle(Kjjeul_in.Substring(0, 2));
+                chapter = int.Parse(Kjjeul_in.Substring(2, 3));
+                verse = int.Parse(Kjjeul_in.Substring(5, 3));
+            }
+        }
+        string bible_display = null;
+        int chapter = -1;
+        int verse = -1;
+
+        // ================================================ 세팅 ================================================
+
+        public VMShowControler()
+        {
+            CDisplayOnOff = new RelayCommand(obj => DisplayVisibility((bool)obj));
+            CTextShowHide = new RelayCommand(obj => TextVisibility((bool)obj));
+            CGoNextPage = new RelayCommand(obj => GoNextPage());
+            CGoPreviousPage = new RelayCommand(obj => GoPreviousPage());
+            CSetDisplayTopMost = new RelayCommand(obj => SetDisplayTopMost());
+        }   
+
+        private void BiblePresentationSetting(string Kjjeul)
+        {
+            this.Kjjeul = Kjjeul;
+
+            Title1 = bible_display;
+            Title2 = chapter + "장 " + verse + "절";
+            OnPropertyChanged("Title1");
+            OnPropertyChanged("Title2");
+
+            BiblePages = new ObservableCollection<string>(
+                module.StringModifier.makeStringPage(
+                    Database.getBible(Kjjeul)
+                    , module.ProgramOption.Bible_CharPerLine
+                    , module.ProgramOption.Bible_LinePerSlide
+                    )
+                );
+            OnPropertyChanged("BiblePages");
+
+            Powerpoint.Bible.Change_BibleChapter(bible_display, chapter.ToString());
+        }
+
+        // ================================================ 이벤트에 쓰일 함수 ================================================
+
+        public void DisplayVisibility(bool OnDisplay)
+        {
+            if (OnDisplay)
+                Powerpoint.Bible.SlideShowRun();
+            else
+                Powerpoint.Bible.SlideShowHide();
+        }
+
+        public void TextVisibility(bool ShowText)
+        {
+            if (ShowText)
+                Powerpoint.Bible.ShowText();
+            else
+                Powerpoint.Bible.HideText();
+        }
+
+        public void SetDisplayTopMost()
+        {
+            Powerpoint.Bible.TopMost();
+        }
+
+        void MovePage(int pageIdx)
+        {
+            CurrentPageIndex = pageIdx;
+            OnPropertyChanged("CurrentPageIndex");
+        }
+
+        public void showBible(string Kjjeul)
+        {
+            BiblePresentationSetting(Kjjeul);
+
+            MovePage(0);
+            OnPropertyChanged("CurrentPageIndex");
+            TextVisibility(true);
+            DisplayVisibility(true);
+        }
+
+        public void GoNextPage()
+        {
+            if (CurrentPageIndex != BiblePages.Count - 1)
+                MovePage(CurrentPageIndex + 1);
+            else
+            {
+                BiblePresentationSetting(Database.getBibleIndex_Next(Kjjeul));
+                MovePage(0);
+
+                // Bible.tempBibleAccesser.applyBibleMoving(Kjjeul.Substring(0, 2), Kjjeul.Substring(2, 3), Kjjeul.Substring(5, 3));
+            }
+        }
+
+        public void GoPreviousPage()
+        {
+            if (CurrentPageIndex != 0)
+                MovePage(CurrentPageIndex - 1);
+            else
+            {
+                BiblePresentationSetting(Database.getBibleIndex_Previous(Kjjeul));
+                if (preview_GoLastPage)
+                    MovePage(BiblePages.Count - 1);
+                else
+                    MovePage(0);
+
+                // Bible.tempBibleAccesser.applyBibleMoving(Kjjeul.Substring(0, 2), Kjjeul.Substring(2, 3), Kjjeul.Substring(5, 3));
+            }
+        }
+
+        int inputedNum = 0;
+        public void NumInput(int num)
+        {
+            inputedNum = inputedNum * 10 + num;
+            if (inputedNum < 0)
+                inputedNum = int.MaxValue;
+        }
+
+        public void NumInput_Enter()
+        {
+            if (inputedNum > 0)
+            {
+                if (inputedNum > BiblePages.Count)
+                    MovePage(BiblePages.Count - 1);
+                else
+                    MovePage(inputedNum - 1);
+
+                inputedNum = 0;
+            }
+        }
+
+        public void NumInput_Remove()
+        {
+            inputedNum = 0;
+        }
     }
 }
