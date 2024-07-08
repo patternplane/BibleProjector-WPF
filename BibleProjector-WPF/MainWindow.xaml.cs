c1b26d54efea477f4679aa77e09263475534b632
patternplane <bss011202@naver.com>
2024-07-09 01:34:44 +0900

    [FEAT] 윈도우 리사이즈 정책 도입 - 이제 프로그램 화면은 리사이징 가능합니다 - 비율의 변화에 따라 내부 화면의 비율도 변화합니다 - 전체적인 크기가 작아지면 내부 화면의 전체적인 크기도 작아집니다 - 사용자의 pc 해상도에 따라 리사이징 최소 크기의 제한이 있습니다

--- BibleProjector-WPF/MainWindow.xaml.cs
+++ BibleProjector-WPF/MainWindow.xaml.cs
@@ -33,7 +33,79 @@ namespace BibleProjector_WPF
             this.SetBinding(shiftEventManagerProperty, new Binding("shiftEventManager"));
             this.SetBinding(capsLockEventManagerProperty, new Binding("capsLockEventManager"));
             this.SetBinding(keyDownEventManagerProperty, new Binding("keyDownEventManager"));
+
+            setMinSize();
+            setInnerContentSize();
         }
+
+        // =================================================== 윈도우 리사이징 처리 ======================================================
+
+        private void setMinSize()
+        {
+            double h = SystemParameters.PrimaryScreenHeight / 2;
+            this.MinHeight = h;
+            this.MinWidth = h / 0.8 * 1.2;
+        }
+
+        private void setInnerContentSize()
+        {
+            double h = WindowContent.ActualHeight;
+            double w = WindowContent.ActualWidth;
+
+            if (h == 0 || w == 0)
+                return;
+
+            double DEFALT_W = 1200.0;
+            double DEFALT_H = 830.0;
+
+            foreach (FrameworkElement f in RatioSizeElements)
+            {
+                if (h / w > DEFALT_H / DEFALT_W)
+                {
+                    f.Height = h / w * DEFALT_W;
+                    f.Width = DEFALT_W;
+                }
+                else
+                {
+                    f.Height = DEFALT_H;
+                    f.Width = w / h * DEFALT_H;
+                }
+            }
+        }
+
+        private void EH_WindowSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            setMinSize();
+            setInnerContentSize();
+        }
+
+        public static readonly DependencyProperty RatioSizeProperty =
+            DependencyProperty.RegisterAttached(
+              "RatioSize",
+              typeof(bool),
+              typeof(MainWindow),
+              new PropertyMetadata(false, RatioSizeCallback)
+            );
+
+        public static bool GetRatioSize(UIElement target) => 
+            (bool)target.GetValue(RatioSizeProperty);
+
+        public static void SetRatioSize(UIElement target, bool value) =>
+            target.SetValue(RatioSizeProperty, value);
+
+        private static void RatioSizeCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is FrameworkElement && (bool)e.NewValue && !RatioSizeElements.Contains(d))
+            {
+                RatioSizeElements.Add((FrameworkElement)d);
+            }
+            else if (d is FrameworkElement && !(bool)e.NewValue && RatioSizeElements.Contains(d))
+            {
+                RatioSizeElements.Remove((FrameworkElement)d);
+            }
+        }
+
+        private static List<FrameworkElement> RatioSizeElements = new List<FrameworkElement>();
 
         // =================================================== 키 상태 광역 전달 ======================================================
 
