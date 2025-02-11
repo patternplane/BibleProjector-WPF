aecb8921a008b8b7cb0f1a6b9bb2472e226352fd
patternplane <bss011202@naver.com>
2025-02-12 01:00:13 +0900

    [FEAT] 검색 화면에서 즉시 가사를 추가하는 기능 추가
    필요성:
    - 자료수정 탭으로 이동하지 않고 새 가사를 추가하는 기능이 요구됨.
    
    기능 내용 :
    - 검색 화면의 미리보기에 곡 추가 버튼이 존재함.
    - 해당 버튼을 눌러 곡 추가 화면으로 전환할 수 있음.
    - 곡 추가 화면에서는 곡의 제목과 가사 내용을 편집 가능.
    - 급한 경우, ESC 키나, 우측 상단 X 버튼으로 잠시 나갈 수도 있으며, 편집 중인 내용은 저장됨.
    - 편집을 완료한 경우 곡 추가 버튼을 눌러 새로운 곡을 등록할 수 있음.
    
    기술 명세 :
    - 곡 추가 화면을 위한 LyricAddView 추가
      - 곡 추가 화면을 구성합니다.
      - 담당 ViewModel : VMLyricAdd
      - ViewMapDictionary에 등록됨.
    - 곡 추가 화면과 기존 검색화면 연결
      - (View) SearchView
        - 해당 화면을 연결하는 Binding이 추가됨.
        - 기존의 "아직 구현되지 않음" 안내 삭제.
      - (ViewModel) VMSearchControl
        - 곡 추가 화면과의 연결을 위한 VMLyricAdd 참조 추가.
        - 곡 추가 작업 처리를 위한 연계 변수/메소드/로직 추가.
        - 기존의 "아직 구현되지 않음" 안내를 위한 속성 삭제.

---
+++ BibleProjector-WPF/View/MainPage/LyricAddView.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BibleProjector_WPF.View.MainPage
+{
+    /// <summary>
+    /// AddLyricView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class LyricAddView : UserControl
+    {
+        public LyricAddView()
+        {
+            InitializeComponent();
+
+            SetBinding(AddNewLyricCommandProperty, new Binding("CAddNewLyric") { Mode = BindingMode.OneWay });
+            SetBinding(CloseCommandProperty, new Binding("CClose") { Mode = BindingMode.OneWay });
+            SetBinding(RemoveLinefeedsCommandProperty, new Binding("CRemoveLinefeed") { Mode = BindingMode.OneWay });
+        }
+
+        // ================== Binding Properties ==================
+
+        public static readonly DependencyProperty AddNewLyricCommandProperty =
+        DependencyProperty.Register(
+            name: "AddNewLyricCommand",
+            propertyType: typeof(ICommand),
+            ownerType: typeof(LyricAddView));
+
+        public ICommand AddNewLyricCommand
+        {
+            get => (ICommand)GetValue(AddNewLyricCommandProperty);
+            set => SetValue(AddNewLyricCommandProperty, value);
+        }
+
+        public static readonly DependencyProperty CloseCommandProperty =
+        DependencyProperty.Register(
+            name: "CloseCommand",
+            propertyType: typeof(ICommand),
+            ownerType: typeof(LyricAddView));
+
+        public ICommand CloseCommand
+        {
+            get => (ICommand)GetValue(CloseCommandProperty);
+            set => SetValue(CloseCommandProperty, value);
+        }
+
+        public static readonly DependencyProperty RemoveLinefeedsCommandProperty =
+        DependencyProperty.Register(
+            name: "RemoveLinefeedsCommand",
+            propertyType: typeof(ICommand),
+            ownerType: typeof(LyricAddView));
+
+        public ICommand RemoveLinefeedsCommand
+        {
+            get => (ICommand)GetValue(RemoveLinefeedsCommandProperty);
+            set => SetValue(RemoveLinefeedsCommandProperty, value);
+        }
+
+        // ================== Event Handlers ==================
+
+        private void EH_AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (AddNewLyricCommand.CanExecute(null))
+            {
+                AddNewLyricCommand.Execute(null);
+                CloseCommand.Execute(null);
+            }
+        }
+
+        private void EH_CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            CloseCommand.Execute(null);
+        }
+
+        private void EH_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                CloseCommand.Execute(null);
+        }
+
+        private void EH_VisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if ((bool)e.NewValue)
+                ((UIElement)sender).Focus();
+        }
+
+        private void EH_RemoveEnterButton_Click(object sender, RoutedEventArgs e)
+        {
+            RemoveLinefeedsCommand.Execute(null);
+        }
+    }
+}
